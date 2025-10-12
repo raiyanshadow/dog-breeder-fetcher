503b86c7cbef7f48c3854b15f0ad16aea823968c
raiyanshadow <taseenraiyan@gmail.com>
2025-10-12 01:05:31 -0400

    CachingBreedFetcher implemented

--- src/main/java/dogapi/CachingBreedFetcher.java
+++ src/main/java/dogapi/CachingBreedFetcher.java
@@ -15,14 +15,24 @@ import java.util.*;
 public class CachingBreedFetcher implements BreedFetcher {
     // TODO Task 2: Complete this class
     private int callsMade = 0;
+    private Map<String, List<String>> cache;
+    private BreedFetcher fetcher;
     public CachingBreedFetcher(BreedFetcher fetcher) {
-
+        this.fetcher = fetcher;
+        cache = new HashMap<>();
     }
 
     @Override
     public List<String> getSubBreeds(String breed) {
-        // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+        List<String> subBreeds;
+        if (cache.containsKey(breed)) {
+            subBreeds = cache.get(breed);
+        } else {
+            callsMade++;
+            subBreeds = fetcher.getSubBreeds(breed);
+            cache.put(breed, subBreeds);
+        }
+        return subBreeds;
     }
 
     public int getCallsMade() {
